c073baecd966907e9cb513511ed4856b0113a9a1
rayen288700 <rayenchuk@gmail.com>
2023-08-22 09:05:23 +0100

    Token

---
+++ token.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * lsh_split_line - this function to split lines
+ * @line: line to split
+ * Return: token
+ */
+
+
+char **lsh_split_line(char *line)
+{
+	int bufsize = BUFFER_SIZE, i = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+	{
+		perror("Allocation error");
+		exit(98);
+	}
+
+	token = strtok(line, DELIME);
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		i++;
+
+		if (i >= bufsize)
+		{
+			bufsize += BUFFER_SIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				perror("Reallocation error");
+				exit(98);
+			}
+		}
+
+		token = strtok(NULL, DELIME);
+	}
+	tokens[i] = NULL;
+	return (tokens);
+}
